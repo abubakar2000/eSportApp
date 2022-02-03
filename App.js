cedfdfd07628c0d382ae007192cfe2d062ec4b44
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-02-03 22:47:14 +0500

    changed name

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={[styles.container]}>
+        <Image source={{uri:'https://randomuser.me/api/portraits/women/10.jpg'}}
+        style={[styles.img]}/>
+        <Text style={[styles.text]}>Ammie</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  text: {
+    fontSize: 30,
+    color: 'darkslateblue'
+  },
+  img: {
+    margin:10,
+    height:100,
+    width:100,
+    borderRadius:100 / 2,
+  }
+})
