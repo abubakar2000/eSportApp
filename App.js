ecb091ea13d0730fa56529f58a831c9be4e76e0b
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-02-03 22:50:34 +0500

    name changed

--- App.js
+++ App.js
@@ -4,29 +4,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <View style={[styles.container]}>
-        <Image source={{uri:'https://randomuser.me/api/portraits/women/10.jpg'}}
-        style={[styles.img]}/>
-        <Text style={[styles.text]}>Ammie</Text>
+      <View>
+        
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    fontSize: 30,
-    color: 'darkslateblue'
-  },
-  img: {
-    margin:10,
-    height:100,
-    width:100,
-    borderRadius:100 / 2,
-  }
+  
 })
